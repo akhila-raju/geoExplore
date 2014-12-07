26b501fc38da6a02f1c95eae08b108c95907afc7
Charlie Pai <mail4charliepai@gmail.com>
2014-12-07 15:58:26 -0800

    Added TextView unfocusing on global touch

--- app/src/main/java/com/geoexplorer/AddPOIActivity.java
+++ app/src/main/java/com/geoexplorer/AddPOIActivity.java
@@ -1,6 +1,7 @@
 package com.geoexplorer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -15,11 +17,19 @@ import android.widget.Toast;
 public class AddPOIActivity extends Activity {
 
     private Button createBtn, backBtn;
+    private LinearLayout globalScreen;
 
     private OnClickListener mCreatePOIClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
             Toast.makeText(getApplicationContext(), "Sup", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private OnClickListener mGlobalClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
         }
     };
 
@@ -28,7 +38,10 @@ public class AddPOIActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_poi);
 
-        createBtn = (Button)findViewById(R.id.create_btn);
+        globalScreen = (LinearLayout)findViewById(R.id.global_screen);
+        globalScreen.setOnClickListener(mGlobalClickListener);
+
+        createBtn = (Button)findViewById(R.id.save_btn);
         createBtn.setOnClickListener(mCreatePOIClickListener);
     }
 
