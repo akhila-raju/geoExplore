b2a320b512f4d316f7cdc508ed41a9159c54e41c
Charlie Pai <mail4charliepai@gmail.com>
2014-12-09 02:17:34 -0800

    Added image gallery upload

--- app/src/main/java/com/geoexplorer/AddPOIActivity.java
+++ app/src/main/java/com/geoexplorer/AddPOIActivity.java
@@ -3,6 +3,10 @@ package com.geoexplorer;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
@@ -12,6 +16,7 @@ import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
@@ -23,8 +28,10 @@ public class AddPOIActivity extends Activity {
 
     private EditText locationName, description;
     private SeekBar difficultyBar;
+    private ImageView imageUpload;
     private Button createBtn, backBtn;
     private LinearLayout globalScreen;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     private OnClickListener mCreatePOIClickListener = new OnClickListener() {
         @Override
@@ -38,6 +45,13 @@ public class AddPOIActivity extends Activity {
             startActivity(intent);
         }
     };
+    private OnClickListener mImageUploadClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, RESULT_LOAD_IMAGE);
+        }
+    };
     private OnClickListener mGlobalClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -46,7 +60,24 @@ public class AddPOIActivity extends Activity {
         }
     };
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            imageUpload.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +90,8 @@ public class AddPOIActivity extends Activity {
         locationName = (EditText)findViewById(R.id.location_name);
         difficultyBar = (SeekBar)findViewById(R.id.difficulty_bar);
         description = (EditText)findViewById(R.id.description);
+        imageUpload = (ImageView)findViewById(R.id.image_upload);
+        imageUpload.setOnClickListener(mImageUploadClickListener);
 
         createBtn = (Button)findViewById(R.id.save_btn);
         createBtn.setOnClickListener(mCreatePOIClickListener);
